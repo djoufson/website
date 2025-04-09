92d701d3e975344c27b369bc7e7d09fdbdbddbd1
Djoufson CHE BENE <djouflegran@gmail.com>
2025-04-09 17:44:15 +0100

    Initial Commit

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="container flex gap-4">
+      <a target='_blank' href="https://github.com/djoufson">Github</a>
+      <a target='_blank' href="https://linkedin.com/in/djoufson">LinkedIn</a>
+      <a target='_blank' href="https://x.com/djoufson">Twitter/X</a>
+    </footer>
+  );
+}
---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import NavBar from "./navbar";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <NavBar />
+      </div>
+    </header>
+  );
+}
