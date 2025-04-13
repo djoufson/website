29805a222d37ad2cfca8048a3ae61b68e7b66bf3
Djoufson CHE BENE <djouflegran@gmail.com>
2025-04-13 11:26:48 +0100

    fix: twitter link

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="container flex gap-4">
       <a target='_blank' href="https://github.com/djoufson">Github</a>
       <a target='_blank' href="https://linkedin.com/in/djoufson">LinkedIn</a>
-      <a target='_blank' href="https://x.com/djoufson">Twitter/X</a>
+      <a target='_blank' href="https://x.com/djouf_legran">Twitter/X</a>
     </footer>
   );
 }
