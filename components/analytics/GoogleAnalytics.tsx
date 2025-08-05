f6e67878bdf51b5b9cfcd0129719d397cb42b9c7
Djoufson CHE BENE <djouflegran@gmail.com>
2025-08-05 02:22:15 +0100

    add Google Analytics

---
+++ components/analytics/GoogleAnalytics.tsx
@@ -0,0 +1,21 @@
+import Script from "next/script";
+
+export default function GoogleAnalytics() {
+  return (
+    <div>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-120LEXYK97"
+        async
+      />
+      <Script>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-120LEXYK97');
+        `}
+      </Script>
+    </div>
+  );
+}
