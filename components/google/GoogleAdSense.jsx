2c120cbbc454df0919f651a7230c65c31d98575c
Djoufson CHE BENE <djouflegran@gmail.com>
2025-08-06 02:25:09 +0100

    use regular jsx to insert google ads script

--- components/google/GoogleAdSense.jsx
+++ components/google/GoogleAdSense.jsx
@@ -2,10 +2,10 @@ import Script from "next/script";
 
 export default function GoogleAdSense() {
   return (
-    <Script
+    <script
       async
-      crossOrigin="anonymous"
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7546228232995495"
-    ></Script>
+      crossorigin="anonymous"
+    ></script>
   );
 }
